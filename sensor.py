38bfffeb32fc4fc7dc293601b0ee78e1e5512266
Vasilis Alivizatos <29864132+alivizatos@users.noreply.github.com>
2025-02-01 11:38:15 +0200

    Update sensor.py
    Change values RINSE and WASH

--- sensor.py
+++ sensor.py
@@ -93,131 +93,136 @@ class CandyBiancaSensor(CoordinatorEntity, SensorEntity):
         self._state = None
         self._attr_extra_state_attributes = {}
         self._update_state()
-        _LOGGER.debug(
-            f"Sensor initialized: {self._attr_name}, unique_id: {self._attr_unique_id}, sensor_type: {self._sensor_type}"
-        )
+        _LOGGER.debug(f"Sensor initialized: {self._attr_name}, unique_id: {self._attr_unique_id}, sensor_type: {self._sensor_type}")
 
     def _update_state(self) -> None:
-        """Update the sensor state from the coordinator data."""
-        _LOGGER.debug(
-            f"Updating state for: {self._attr_name}, sensor_type: {self._sensor_type}"
-        )
-        if self.coordinator.json_data:
-            status_data = self.coordinator.json_data.get(self._device_type, {})
-            _LOGGER.debug(f"Status data: {status_data}")
-            self._state = status_data.get(self._sensor_type)
-            _LOGGER.debug(f"Raw state: {self._state}")
+            """Update the sensor state from the coordinator data."""
+            _LOGGER.debug(f"Updating state for: {self._attr_name}, sensor_type: {self._sensor_type}")
+            if self.coordinator.json_data:
+                status_data = self.coordinator.json_data.get(self._device_type, {})
+                _LOGGER.debug(f"Status data: {status_data}")
+                self._state = status_data.get(self._sensor_type)
+                _LOGGER.debug(f"Raw state: {self._state}")
 
-            if self._sensor_type == "StatoWiFi":
-                if self._state == "1":
-                    self._state = "Remote Control"
-                elif self._state == "0":
-                    self._state = "No Remote Control"
+                if self._sensor_type == "StatoWiFi":
+                    if self._state == "1":
+                        self._state = "Remote Control"
+                    elif self._state == "0":
+                         self._state = "No Remote Control"
+                    else:
+                        self._state = "Unknown"
+                    _LOGGER.debug(f"Translated StatoWiFi state: {self._state}")
+                if self._sensor_type == "CodiceErrore":
+                    if self._state == "E0":
+                        self._state = "Healthy"
+                    else:
+                        _LOGGER.debug(f"Error code: {self._state}")
+                        
+                    _LOGGER.debug(f"Translated error code state: {self._state}")
+                if self._sensor_type == "StatoDWash":
+                    states = {
+                        "0": "IDLE",
+                        "1": "PRE_WASH",
+                        "2": "WASH",
+                        "3": "RINSE",
+                        "4": "DRYING",
+                        "5": "FINISHED",
+                    }
+                    self._state = states.get(self._state, self._state)
+                    _LOGGER.debug(f"Translated StatoDWash state: {self._state}")
+                if self._sensor_type == "StatoLavatrice":
+                    states = {
+                        "0": "IDLE",
+                        "1": "PRE_WASH",
+                        "2": "WASH",
+                        "3": "RINSE",
+                        "4": "SPIN",
+                        "5": "FINISHED",
+                    }
+                    self._state = states.get(self._state, self._state)
+                    _LOGGER.debug(f"Translated StatoLavatrice state: {self._state}")
+                if self._sensor_type == "MissSalt":
+                    if self._state == "0":
+                        self._state = "Salt OK"
+                    elif self._state == "1":
+                        self._state = "Salt Missing"
+                    _LOGGER.debug(f"Translated MissSalt state: {self._state}")
+                if self._sensor_type == "MissRinse":
+                    if self._state == "0":
+                        self._state = "Rinse OK"
+                    elif self._state == "1":
+                         self._state = "Rinse Missing"
+                    _LOGGER.debug(f"Translated MissRinse state: {self._state}")
+                if self._sensor_type == "TreinUno":
+                    if self._state == "0":
+                        self._state = "Disabled"
+                    elif self._state == "1":
+                        self._state = "Enabled"
+                    _LOGGER.debug(f"Translated TreinUno state: {self._state}")
+                if self._sensor_type == "Eco":
+                    if self._state == "0":
+                        self._state = "Disabled"
+                    elif self._state == "1":
+                        self._state = "Enabled"
+                    _LOGGER.debug(f"Translated Eco state: {self._state}")
+                if self._sensor_type == "ExtraDry":
+                    if self._state == "0":
+                        self._state = "Disabled"
+                    elif self._state == "1":
+                        self._state = "Enabled"
+                    _LOGGER.debug(f"Translated ExtraDry state: {self._state}")
+                if self._sensor_type == "OpenDoor":
+                     if self._state == "0":
+                        self._state = "Closed"
+                     elif self._state == "1":
+                        self._state = "Open"
+                     _LOGGER.debug(f"Translated OpenDoor state: {self._state}")
+                if self._sensor_type == "MetaCarico":
+                    if self._state == "0":
+                        self._state = "Full Load"
+                    elif self._state == "1":
+                        self._state = "Half Load"
+                    _LOGGER.debug(f"Translated MetaCarico state: {self._state}")
+                if self._sensor_type == "Program":
+                    if self._state == "P19":
+                        self._state = "Zoom 39mins 60°C"
+                    elif self._state == "P2":
+                        self._state = "P1 75°C"
+                    elif self._state == "P5":
+                        self._state = "Universal 60°C"
+                    elif self._state == "P8":
+                        self._state = "ECO 45°C"
+                    elif self._state == "P12":
+                         self._state = "PreWash 5mins"
+                    _LOGGER.debug(f"Translated Program state: {self._state}")
+                if self._sensor_type == "RemTime":
+                    try:
+                        minutes = int(self._state)
+                        hours = minutes // 60
+                        minutes = minutes % 60
+                        self._state = f"{hours} hours {minutes} minutes"
+                    except (ValueError, TypeError):
+                         _LOGGER.error(f"Error converting RemTime value: {self._state}")
+                         self._state = "Error"
+                    _LOGGER.debug(f"Translated RemTime state: {self._state}")
                 else:
-                    self._state = "Unknown"
-                _LOGGER.debug(f"Translated StatoWiFi state: {self._state}")
-            if self._sensor_type == "CodiceErrore":
-                if self._state == "E0":
-                    self._state = "Healthy"
-                else:
-                    _LOGGER.debug(f"Error code: {self._state}")
-
-                _LOGGER.debug(f"Translated error code state: {self._state}")
-            if self._sensor_type == "StatoDWash":
-                states = {
-                    "0": "IDLE",
-                    "1": "PRE_WASH",
-                    "2": "WASH",
-                    "3": "RINSE",
-                    "4": "DRYING",
-                    "5": "FINISHED",
-                }
-                self._state = states.get(self._state, self._state)
-                _LOGGER.debug(f"Translated StatoDWash state: {self._state}")
-            if self._sensor_type == "StatoLavatrice":
-                states = {
-                    "0": "IDLE",
-                    "1": "PRE_WASH",
-                    "2": "WASH",
-                    "3": "RINSE",
-                    "4": "SPIN",
-                    "5": "FINISHED",
-                }
-                self._state = states.get(self._state, self._state)
-                _LOGGER.debug(f"Translated StatoLavatrice state: {self._state}")
-            if self._sensor_type == "MissSalt":
-                if self._state == "0":
-                    self._state = "Salt OK"
-                elif self._state == "1":
-                    self._state = "Salt Missing"
-                _LOGGER.debug(f"Translated MissSalt state: {self._state}")
-            if self._sensor_type == "MissRinse":
-                if self._state == "0":
-                    self._state = "Rinse OK"
-                elif self._state == "1":
-                    self._state = "Rinse Missing"
-                _LOGGER.debug(f"Translated MissRinse state: {self._state}")
-            if self._sensor_type == "TreinUno":
-                if self._state == "0":
-                    self._state = "Disabled"
-                elif self._state == "1":
-                    self._state = "Enabled"
-                _LOGGER.debug(f"Translated TreinUno state: {self._state}")
-            if self._sensor_type == "Eco":
-                if self._state == "0":
-                    self._state = "Disabled"
-                elif self._state == "1":
-                    self._state = "Enabled"
-                _LOGGER.debug(f"Translated Eco state: {self._state}")
-            if self._sensor_type == "ExtraDry":
-                if self._state == "0":
-                    self._state = "Disabled"
-                elif self._state == "1":
-                    self._state = "Enabled"
-                _LOGGER.debug(f"Translated ExtraDry state: {self._state}")
-            if self._sensor_type == "OpenDoor":
-                if self._state == "0":
-                    self._state = "Closed"
-                elif self._state == "1":
-                    self._state = "Open"
-                _LOGGER.debug(f"Translated OpenDoor state: {self._state}")
-            if self._sensor_type == "MetaCarico":
-                if self._state == "0":
-                    self._state = "Full Load"
-                elif self._state == "1":
-                    self._state = "Half Load"
-                _LOGGER.debug(f"Translated MetaCarico state: {self._state}")
-            if self._sensor_type == "Program":
-                if self._state == "P19":
-                    self._state = "Zoom 39mins 60°C"
-                elif self._state == "P2":
-                    self._state = "P1 75°C"
-                elif self._state == "P5":
-                    self._state = "Universal 60°C"
-                elif self._state == "P8":
-                    self._state = "ECO 45°C"
-                elif self._state == "P12":
-                    self._state = "PreWash 5mins"
-                _LOGGER.debug(f"Translated Program state: {self._state}")
+                   _LOGGER.debug(f"No translation needed for sensor: {self._sensor_type}")
             else:
-                _LOGGER.debug(f"No translation needed for sensor: {self._sensor_type}")
-        else:
-            self._state = "error"
-            self._attr_extra_state_attributes = {"error": "Data not available"}
-            _LOGGER.debug(
-                f"Sensor state error: {self._attr_name}, state: {self._state}"
-            )
+                 self._state = "error"
+                 self._attr_extra_state_attributes = {"error": "Data not available"}
+                 _LOGGER.debug(f"Sensor state error: {self._attr_name}, state: {self._state}")
+
 
     @property
     def native_value(self) -> StateType:
         """Return the state of the sensor."""
         return self._state
-
+    
     @property
     def available(self) -> bool:
         """Return if entity is available."""
         return self.coordinator.last_update_success
-
+    
     async def async_added_to_hass(self) -> None:
         """Connect to dispatcher listening for entity data notifications."""
         await super().async_added_to_hass()
@@ -225,6 +230,7 @@ class CandyBiancaSensor(CoordinatorEntity, SensorEntity):
             self.coordinator.async_add_listener(self._handle_coordinator_update)
         )
         _LOGGER.debug(f"Listener added to coordinator: {self._attr_name}")
+
 
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
